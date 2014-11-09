71f7c9361db322d93b8788d1e08e20a0fa4362ee
Sergey Toy <toydestroyer@me.com>
2014-11-09 11:00:59 +0300

    favicon fixes after path changing

--- javascripts/application.js
+++ javascripts/application.js
@@ -6,10 +6,10 @@
 
   var favicons = [
     '/favicon.ico?v2',
-    '/img/favicons/01.ico',
-    '/img/favicons/02.ico',
-    '/img/favicons/03.ico',
-    '/img/favicons/04.ico',
+    '/images/favicons/01.ico',
+    '/images/favicons/02.ico',
+    '/images/favicons/03.ico',
+    '/images/favicons/04.ico',
   ];
 
   (function favicon(i) {
